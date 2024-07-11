6d7acaa904390fd4ac8bd4e1231c1a4b0c1723de
Ashanti Hatchett <ahatchett02@gmail.com>
2024-07-11 03:25:24 +0000

    updated

--- chat.rb
+++ chat.rb
@@ -22,4 +22,32 @@ api_response = client.chat(
   }
 )
 
-pp api_response
+next_message = api_response.fetch("choices").at(0).fetch("message")
+
+message_list.push(next_message)
+
+message_list.push(
+  {:role => "user", :content => "What about NYC?"}
+)
+
+api_response = client.chat(
+  parameters: {
+    model: "gpt-3.5-turbo",
+    messages: message_list
+  }
+)
+
+next_message = api_response.fetch("choices").at(0).fetch("message")
+
+message_list.push(next_message)
+
+input = ""
+
+while input != "bye"
+  puts "Hello! How can I help you today?"
+  puts "-" * 50
+
+  input = gets.chomp
+
+  chat_with_retry(client, message_list)
+end
